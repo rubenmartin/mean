de9f509c593f7a243ef628a42e28399098e6967b
Ruben Martin <ruben.martin.ramos@gmail.com>
2014-11-09 20:03:36 +0100

    load alll!
    a Please enter the commit message for your changes. Lines starting

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,27 @@
 exports.render = function(req,res){
+	// checking if the variable lastVisit was there configured if not add a new one
+	console.log("index.server.controller.js");
+
+	if(req.session.lastVisit){
+		console.log("lastVisit:"+req.session.lastVisit);
+	}
+
+	req.session.lastVisit = new Date();
+
+	// We draw the page and their variable
+
+	res.render('index', {
+		title:'Hola Mundos'
+	});
+
+};
+
+
+/**
+
+exports.render = function(req,res){
 	res.send('Hola Mundo');
 };
+
+**/
+
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var express = require('express');
+// we force the value on the env.NODE_ENV by default to development
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+//Initial cnfguration  for express and mongoose
+
+var mongoose = require('./config/mongoose'),  
+			express = require('./config/express');
+var db = mongoose();
 var app = express();
-
-app.use('/' , function(req,res){
-	res.send('Hola Mundo');
-});
-
 app.listen(3000);
-
-console.log('ejecustnadoes servidor puerto 3000');
-
 module.exports = app;
+
+console.log('Servidor Ejecutandose en local en el puerto 3000');
+
+
+
+// var express = require('express');
+// var app = express();
+
+// app.use('/' , function(req,res){
+// 	res.send('Hola Mundo');
+// });
+
+// app.listen(3000);
+
+// console.log('ejecustnadoes servidor puerto 3000');
+
+// module.exports = app;
 
 	
 	
